7a5197dc74d90f6b291fdfe7d0dbc8df84b99613
Ermes Liriano <ermesliriano@mercedarios.net>
2023-08-28 19:47:21 +0200

    Esta es la segunda versión del Hola Mundo del Temporizador (2.0)

---
+++ Temporizador/src/main/java/edu/mentor/temporizador/Temporizador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.mentor.temporizador;
+
+/**
+ *
+ * @author Ermes
+ */
+public class Temporizador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hola Mundo 2.0!");
+    }
+}
