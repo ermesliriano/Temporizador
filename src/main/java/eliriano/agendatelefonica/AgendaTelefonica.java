3c0adccefe7f584cf2377f508603a1bfb8e0b31d
Ermes Liriano <ermesliriano@mercedarios.net>
2023-09-07 22:32:06 +0200

    Carga inicial

---
+++ src/main/java/eliriano/agendatelefonica/AgendaTelefonica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package eliriano.agendatelefonica;
+
+/**
+ *
+ * @author Ermes
+ */
+public class AgendaTelefonica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
