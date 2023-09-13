e75574b790d009de05c5eda09835fb84f29b1b25
Ermes Liriano <ermesliriano@mercedarios.net>
2023-09-13 18:28:16 +0200

    Carga inicial del GUI y de otros paquetes en blanco

--- Temporizador/src/main/java/juanjo_ermes_team/temporizador/Temporizador.java
+++ Temporizador/src/main/java/juanjo_ermes_team/temporizador/Temporizador.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package edu.mentor.temporizador;
+package juanjo_ermes_team.temporizador;
 
 /**
  *
